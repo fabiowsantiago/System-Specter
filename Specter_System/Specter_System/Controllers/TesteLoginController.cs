fac888594d68cb41dec284ddedee7a81f146d84d
fabiowsantiago <fabiowsantiago@hotmail.com>
2021-04-13 22:36:29 -0300

    Version-13042021

---
+++ Specter_System/Specter_System/Controllers/TesteLoginController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Specter_System.Controllers
+{
+    public class TesteLoginController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
